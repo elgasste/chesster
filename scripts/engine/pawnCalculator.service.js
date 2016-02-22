89107b06ab327a1114159584fc0d23d2dcc94c71
Steve Elgas <s.elgas@techsmith.com>
2016-02-22 16:05:34 -0500

    added white pawn move calculations

--- scripts/engine/pawnCalculator.service.js
+++ scripts/engine/pawnCalculator.service.js
@@ -10,18 +10,15 @@
                 return $q.when([]);
             }
             var possibleMoves = [];
-            var obstruction = '';
 
             // non-capturing moves
-            if (fromSquare < 56) {
-                obstruction = position.pieces[fromSquare + 8];
-                if (obstruction == '-') {
-                    possibleMoves.push(fromSquare + 8);
-                    if (fromSquare > 7 && fromSquare < 16) {
-                        obstruction = position.pieces[fromSquare + 16];
-                        if (obstruction == '-') {
-                            possibleMoves.push(fromSquare + 16);
-                        }
+            var obstruction = position.pieces[fromSquare + 8];
+            if (obstruction == '-') {
+                possibleMoves.push(fromSquare + 8);
+                if (fromSquare > 7 && fromSquare < 16) {
+                    obstruction = position.pieces[fromSquare + 16];
+                    if (obstruction == '-') {
+                        possibleMoves.push(fromSquare + 16);
                     }
                 }
             }
@@ -52,8 +49,46 @@
         };
 
         var getWhiteMoves = function(fromSquare) {
-            // TODO
-            return $q.when([]);
+            if (fromSquare < 8) {
+                return $q.when([]);
+            }
+            var possibleMoves = [];
+
+            // non-capturing moves
+            var obstruction = position.pieces[fromSquare - 8];
+            if (obstruction == '-') {
+                possibleMoves.push(fromSquare - 8);
+                if (fromSquare > 47 && fromSquare < 56) {
+                    obstruction = position.pieces[fromSquare - 16];
+                    if (obstruction == '-') {
+                        possibleMoves.push(fromSquare - 16);
+                    }
+                }
+            }
+
+            // capturing moves
+            if (fromSquare % 8 != 0) {
+                obstruction = position.pieces[fromSquare - 7];
+                if (obstruction != '-' && obstruction.toLowerCase() == obstruction) {
+                    possibleMoves.push(fromSquare - 7);
+                }
+            }
+            if ((fromSquare + 1) % 8 != 0) {
+                obstruction = position.pieces[fromSquare - 9];
+                if (obstruction != '-' && obstruction.toLowerCase() == obstruction) {
+                    possibleMoves.push(fromSquare - 9);
+                }
+            }
+
+            // en passant
+            if (position.passant != '-' && fromSquare > 23 && fromSquare < 32) {
+                var passantSquare = algebraicHelper.getSquareIndexFromAlgebraic(position.passant);
+                if (passantSquare == fromSquare - 7 || passantSquare == fromSquare - 9) {
+                    possibleMoves.push(passantSquare);
+                }
+            }
+
+            return $q.when(possibleMoves);
         };
 
         var getPossibleMovesFromSquare = function(pos, fromSquare) {
