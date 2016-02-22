99044535e168974faed80eb4aad750e9e44427c4
Steve Elgas <s.elgas@techsmith.com>
2016-02-22 17:24:51 -0500

    added and implemented bishop move calculator

---
+++ scripts/engine/bishopCalculator.service.js
@@ -0,0 +1,87 @@
+'use strict';
+(function(angular) {
+
+    angular.module('chesster.engine').factory('bishopCalculator', ['$q', 'indexHelper', function ($q, indexHelper) {
+
+        var position = {};
+        var possibleMoves = [];
+
+        var addMoveIfPossible = function(targetSquare, color) {
+            var obstruction = position.pieces[targetSquare];
+            if (obstruction == '-' || (color == 'b' && obstruction.toUpperCase() == obstruction) || (color == 'w' && obstruction.toLowerCase() == obstruction)) {
+                possibleMoves.push(targetSquare);
+            }
+        };
+
+        var getMovesForColor = function(fromSquare, color) {
+            var file = indexHelper.getFileFromIndex(fromSquare);
+
+            var upperPathClear = true;
+            var lowerPathClear = true;
+            for (var i = file - 1, iter = 1; (i > 0) && (upperPathClear || lowerPathClear); i--, iter++) {
+                if (upperPathClear) {
+                    var upperLeft = fromSquare - (9 * iter);
+                    if (upperLeft >= 0) {
+                        addMoveIfPossible(upperLeft, color);
+                        upperPathClear = position.pieces[upperLeft] == '-';
+                    } else {
+                        upperPathClear = false;
+                    }
+                }
+                if (lowerPathClear) {
+                    var lowerLeft = fromSquare + (7 * iter);
+                    if (lowerLeft <= 63) {
+                        addMoveIfPossible(lowerLeft, color);
+                        lowerPathClear = position.pieces[lowerLeft] == '-';
+                    } else {
+                        lowerPathClear = false;
+                    }
+                }
+            }
+
+            upperPathClear = true;
+            lowerPathClear = true;
+            for (var i = file + 1, iter = 1; (i < 9) && (upperPathClear || lowerPathClear); i++, iter++) {
+                if (upperPathClear) {
+                    var upperRight = fromSquare - (7 * iter);
+                    if (upperRight >= 0) {
+                        addMoveIfPossible(upperRight, color);
+                        upperPathClear = position.pieces[upperRight] == '-';
+                    } else {
+                        upperPathClear = false;
+                    }
+                }
+                if (lowerPathClear) {
+                    var lowerRight = fromSquare + (9 * iter);
+                    if (lowerRight <= 63) {
+                        addMoveIfPossible(lowerRight, color);
+                        lowerPathClear = position.pieces[lowerRight] == '-';
+                    } else {
+                        lowerPathClear = false;
+                    }
+                }
+            }
+
+            return $q.when(possibleMoves);
+        };
+
+        var getPossibleMovesFromSquare = function(pos, fromSquare) {
+            position = pos;
+            possibleMoves = [];
+            var piece = position.pieces[fromSquare];
+            if (piece == 'b') {
+                return getMovesForColor(fromSquare, 'b');
+            } else if (piece == 'B') {
+                return getMovesForColor(fromSquare, 'w');
+            } else {
+                return $q.when([]);
+            }
+        };
+
+        return {
+            getPossibleMovesFromSquare: getPossibleMovesFromSquare
+        };
+
+    }]);
+
+})(angular);
--- scripts/engine/indexHelper.service.js
+++ scripts/engine/indexHelper.service.js
@@ -11,9 +11,14 @@
             return index - (8 * (parseInt(index / 8))) + 1;
         };
 
+        var getIndexFromRankAndFile = function(rank, file) {
+            return (8 * (8 - rank)) + (file - 1);
+        };
+
         return {
             getRankFromIndex: getRankFromIndex,
-            getFileFromIndex: getFileFromIndex
+            getFileFromIndex: getFileFromIndex,
+            getIndexFromRankAndFile: getIndexFromRankAndFile
         };
 
     }]);
