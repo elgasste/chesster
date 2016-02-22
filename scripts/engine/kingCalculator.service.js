b9f3231ad31885694ebfb596657ac8dbe5a902c8
Steve Elgas <s.elgas@techsmith.com>
2016-02-22 18:22:16 -0500

    implemented kingside castling calculation

--- scripts/engine/kingCalculator.service.js
+++ scripts/engine/kingCalculator.service.js
@@ -14,6 +14,33 @@
             }
         };
 
+        var tryKingsideCastling = function(fromSquare, color) {
+            var safetySquares = [];
+            if (color == 'b') {
+                if (position.pieces[4] != 'k' || position.pieces[7] != 'r' || position.pieces[5] != '-' || position.pieces[6] != '-') {
+                    return;
+                }
+                safetySquares = [4, 5, 6];
+            } else {
+                if (position.pieces[60] != 'K' || position.pieces[63] != 'R' || position.pieces[61] != '-' || position.pieces[62] != '-') {
+                    return;
+                }
+                safetySquares = [60, 61, 62];
+            }
+
+            for (var i = 0; i < safetySquares.length; i++) {
+                if (dangerZones.indexOf(safetySquares[i]) != -1) {
+                    return;
+                }
+            }
+
+            possibleMoves.push(color == 'b' ? 6 : 62);
+        };
+
+        var tryQueensideCastling = function(fromSquare, color) {
+            // TODO
+        };
+
         var getMovesForColor = function(fromSquare, color) {
             var rank = indexHelper.getRankFromIndex(fromSquare);
             var file = indexHelper.getFileFromIndex(fromSquare);
@@ -31,6 +58,14 @@
             }
             if (file > 1) addMoveIfPossible(fromSquare - 1, color);
             if (file < 8) addMoveIfPossible(fromSquare + 1, color);
+
+            // castling moves
+            if (position.castling.indexOf(color == 'w' ? 'K' : 'k') != -1) {
+                tryKingsideCastling(fromSquare, color);
+            }
+            if (position.castling.indexOf(color == 'w' ? 'Q' : 'q') != -1) {
+                tryQueensideCastling(fromSquare, color);
+            }
 
             return $q.when(possibleMoves);
         };
