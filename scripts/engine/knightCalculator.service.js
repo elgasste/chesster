9510dd6e6fb1c3048d4d5ee6a9c24d6717510d03
Steve Elgas <s.elgas@techsmith.com>
2016-02-22 16:47:16 -0500

    implemented black knight move calculations

--- scripts/engine/knightCalculator.service.js
+++ scripts/engine/knightCalculator.service.js
@@ -1,24 +1,50 @@
 'use strict';
 (function(angular) {
 
-    angular.module('chesster.engine').factory('knightCalculator', ['$q', function ($q) {
+    angular.module('chesster.engine').factory('knightCalculator', ['$q', 'indexHelper', function ($q, indexHelper) {
 
         var position = {};
+        var possibleMoves = [];
+
+        var addBlackMoveIfPossible = function(targetSquare) {
+            var obstruction = position.pieces[targetSquare];
+            if (obstruction == '-' || obstruction.toUpperCase() == obstruction) {
+                possibleMoves.push(targetSquare);
+            }
+        };
 
         var getBlackMoves = function(fromSquare) {
-            // TODO
-            var possibleMoves = [];
+            var fromRank = indexHelper.getRankFromIndex(fromSquare);
+            var fromFile = indexHelper.getFileFromIndex(fromSquare);
+
+            if (fromRank < 8) {
+                if (fromFile > 1) addBlackMoveIfPossible(fromSquare - 10);
+                if (fromFile < 7) addBlackMoveIfPossible(fromSquare - 6);
+            }
+            if (fromRank < 7) {
+                if (fromFile > 1) addBlackMoveIfPossible(fromSquare - 17);
+                if (fromFile < 8) addBlackMoveIfPossible(fromSquare - 15);
+            }
+            if (fromRank > 2) {
+                if (fromFile > 1) addBlackMoveIfPossible(fromSquare + 15);
+                if (fromFile < 8) addBlackMoveIfPossible(fromSquare + 17);
+            }
+            if (fromRank > 1) {
+                if (fromFile > 1) addBlackMoveIfPossible(fromSquare + 6);
+                if (fromFile < 7) addBlackMoveIfPossible(fromSquare + 10);
+            }
+
             return $q.when(possibleMoves);
         };
 
         var getWhiteMoves = function(fromSquare) {
             // TODO
-            var possibleMoves = [];
             return $q.when(possibleMoves);
         };
 
         var getPossibleMovesFromSquare = function(pos, fromSquare) {
             position = pos;
+            possibleMoves = [];
             var piece = position.pieces[fromSquare];
             if (piece == 'n') {
                 return getBlackMoves(fromSquare);
